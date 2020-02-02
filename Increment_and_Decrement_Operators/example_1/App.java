42b20e6c5e5e8ad6b13873b0aa7c32ddfe1676bf
Filipe Canatto <canatto.filipe@gmail.com>
2020-02-02 13:24:48 -0300

    pre-increment and pos-increment example

---
+++ Increment_and_Decrement_Operators/example_1/App.java
@@ -0,0 +1,16 @@
+package example_1;
+
+public class App{
+
+	public static void main(String[] args){
+
+		int x = 10;
+		System.out.println(++x); // Outputs 11
+
+		int y = 10;
+		System.out.println(--x); // Outputs 10
+	
+
+	}
+
+}
---
+++ Increment_and_Decrement_Operators/example_2/App.java
@@ -0,0 +1,19 @@
+package example_2;
+
+public class App{
+
+	public static void main(String[] args){
+		
+		int x = 10;
+
+		System.out.println(x++); // Outputs 10
+		System.out.println(x); // Outputs 11
+
+		int y = 10;
+
+		System.out.println(y--); // Outputs 10
+		System.out.println(y); // Outputs 9
+			
+	}
+
+}
